6f6b97b78583b234413828b62114d85d268783b8
Julian Dunskus <julian.dunskus@gmail.com>
2020-07-28 03:59:15 +0200

    improve log feedback for failed assertions

--- src/test/util.test.ts
+++ src/test/util.test.ts
@@ -6,11 +6,11 @@ suite("util", () => {
     suite("Location", () => {
         test("Basics", async () => {
             const root = new Location("/tmp/folder");
-            assert(root.basePath === "/tmp/folder");
-            assert(`${root.enclosingFolder}` === "Location(/tmp)");
-            assert(`${root.child("sub", "path")}` === "Location(/tmp/folder/sub/path)");
-            assert(root.path("sub", "path") === "/tmp/folder/sub/path");
-            assert(root.executable("run") === "/tmp/folder/run" || root.executable("run") === "/tmp/folder/run.exe");
+            assert.strictEqual(root.basePath, "/tmp/folder");
+            assert.strictEqual(`${root.enclosingFolder}`, "Location(/tmp)");
+            assert.strictEqual(`${root.child("sub", "path")}`, "Location(/tmp/folder/sub/path)");
+            assert.strictEqual(root.path("sub", "path"), "/tmp/folder/sub/path");
+            assert(root.executable("run") === "/tmp/folder/run" || root.executable("run") === "/tmp/folder/run.exe", `unexpected executable path: ${root.executable("run")}`);
             await root.mkdir(); // ensures the folder exists
             assert(await root.exists());
         });
