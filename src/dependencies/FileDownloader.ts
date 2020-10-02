e3653054f07771578e325d963ff0494c84da1952
Linard Arquint <linard.arquint@inf.ethz.ch>
2020-10-02 10:53:33 +0200

    adds support for downloading https files and adds corresponding test cases

--- src/dependencies/FileDownloader.ts
+++ src/dependencies/FileDownloader.ts
@@ -1,6 +1,8 @@
 import * as http from 'http';
+import * as https from 'https';
 import * as fs from 'fs-extra';
 import * as path from 'path';
+import { URL } from 'url';
 
 import { DependencyInstaller, Location, ProgressListener } from '..';
 
@@ -22,7 +24,7 @@ export class FileDownloader implements DependencyInstaller {
 
 		try {
 			await new Promise((resolve, reject) => {
-				http.get(this.remoteUrl, (response) => {
+				this.get(this.remoteUrl, (response: http.IncomingMessage) => {
 					if (response.statusCode !== 200) {
 						reject(`request to ${this.remoteUrl} failed with status code ${response.statusCode}`);
 					}
@@ -55,6 +57,23 @@ export class FileDownloader implements DependencyInstaller {
 		} catch (e) {
 			await temp.unlinkIfExists();
 			throw e;
+		}
+	}
+
+	/** 
+	 * wrapper for http.get and https.get
+	 * this is a text book example for a bad library especially since http.get and https.get share 
+	 * the same callback argument and return type
+	 */
+	private get(url: string, callback?: (res: http.IncomingMessage) => void): http.ClientRequest {
+		const prot: string = new URL(url).protocol;
+		switch (prot) {
+			case "http:":
+				return http.get(url, callback);
+			case "https:":
+				return https.get(url, callback);
+			default:
+				throw `unknown protocol '${prot}'`;
 		}
 	}
 }
--- src/test/dependencies.test.ts
+++ src/test/dependencies.test.ts
@@ -1,8 +1,27 @@
+import * as path from 'path';
+import * as fs from 'fs';
 import * as assert from 'assert';
 
 import { Dependency, FileDownloader, InstallerSequence, LocalReference, ZipExtractor } from '..';
+import { withProgressInWindow } from '../util';
 
 suite("dependencies", () => {
+
+    const PROJECT_ROOT = path.join(__dirname, "../../");
+    const TMP_PATH: string = path.join(PROJECT_ROOT, "src", "test", "tmp");
+    const HTTP_DOWNLOAD_URL: string = "http://viper.ethz.ch/downloads/ViperToolsLinux.zip";
+    const HTTP_DOWNLOAD_TIMEOUT_MS: number = 60 * 1000;
+    const HTTPS_DOWNLOAD_URL: string = 
+        "https://ethz.ch/content/dam/ethz/special-interest/infk/chair-program-method/pm/documents/Education/Theses/Julian_Dunskus_BA_Description.pdf";
+    const HTTPS_DOWNLOAD_TIMEOUT_MS: number = 1000;
+
+    suiteSetup(function() {
+        // create a tmp directory for downloading files to it
+        if (!fs.existsSync(TMP_PATH)) {
+            fs.mkdirSync(TMP_PATH);
+        }
+    });
+
     test("Dependency Definition", () => {
         const myDependency = new Dependency<"remote" | "local">(
             "/path/to/install/folder",
@@ -17,5 +36,40 @@ suite("dependencies", () => {
         // just demonstrating that it doesn't crash; this is the example from the readme.
     });
 
-    // TODO test the actual installer components.
+    test("HTTP Download", function() {
+        this.timeout(HTTP_DOWNLOAD_TIMEOUT_MS);
+        const myDependency = new Dependency<"remote">(
+            TMP_PATH,
+            ["remote",
+              new InstallerSequence([
+                    new FileDownloader(HTTP_DOWNLOAD_URL)
+              ])
+            ]
+          );
+        return withProgressInWindow(
+            "Testing download of an HTTP file",
+            listener => myDependency.install("remote", true, listener));
+    });
+
+    test("HTTPS Download", function() {
+        this.timeout(HTTPS_DOWNLOAD_TIMEOUT_MS);
+        const myDependency = new Dependency<"remote">(
+            TMP_PATH,
+            ["remote",
+              new InstallerSequence([
+                    new FileDownloader(HTTPS_DOWNLOAD_URL)
+              ])
+            ]
+          );
+        return withProgressInWindow(
+            "Testing download of an HTTP file",
+            listener => myDependency.install("remote", true, listener));
+    });
+
+    suiteTeardown(function() {
+        // delete tmp directory containing downloaded files:
+        if (fs.existsSync(TMP_PATH)) {
+            fs.rmdirSync(TMP_PATH, { recursive: true });
+        }
+    });
 });
